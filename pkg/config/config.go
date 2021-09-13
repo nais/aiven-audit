238a0a742e5c1055f5fa26e6ec01cad4a50f2cbb
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-09-13 23:34:47 +0200

    Add db and projects config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,17 +1,25 @@
 package config
 
 import "os"
+import "strings"
 
 type AivenAuditConfig struct {
 	AivenAPIToken string
 	AuditLogAddr  string
+	DbHost        string
+	Projects      []string
 }
 
 func ConfigFromEnv() AivenAuditConfig {
 	aivenPat := os.Getenv("AIVEN_AUDIT_PAT")
 	auditAddr := os.Getenv("AUDIT_LOG_ADDR")
+	dbHost := os.Getenv("NAIS_DATABASE_AIVENAUDIT_EVENTHASHDB_URL")
+	strProjects := os.Getenv("AIVEN_PROJECTS")
+	projects := strings.Split(strProjects, " ")
 	return AivenAuditConfig{
 		aivenPat,
 		auditAddr,
+		dbHost,
+		projects,
 	}
 }
