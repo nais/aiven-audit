92b683612df6c99df4c1c9c5d54c7fe67e460155
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-09-13 23:35:25 +0200

    Add support for transactions

--- pkg/aiven_sync/database.go
+++ pkg/aiven_sync/database.go
@@ -1,6 +1,7 @@
 package aiven_sync
 
 import (
+	"context"
 	"database/sql"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	log "github.com/sirupsen/logrus"
@@ -8,6 +9,7 @@ import (
 
 type SyncedEventsStore struct {
 	db *sql.DB
+	tx *sql.Tx
 }
 
 func NewSyncedEventsStore(dbUrl string) SyncedEventsStore {
@@ -19,25 +21,41 @@ func NewSyncedEventsStore(dbUrl string) SyncedEventsStore {
 
 	return SyncedEventsStore{
 		db: db,
+		tx: nil,
 	}
 }
 
-func (ses SyncedEventsStore) Init() {
+func (ses *SyncedEventsStore) Init() {
 	_, err := ses.db.Exec(DbSchema)
 	if err != nil {
 		log.Fatalf("Error while initializing db: %v", err)
 	}
 }
 
-func (ses SyncedEventsStore) Close() {
+func (ses *SyncedEventsStore) Close() {
 	err := ses.db.Close()
 	if err != nil {
 		log.Errorf("Error while closing db: %v", err)
 	}
 }
 
-func (ses SyncedEventsStore) LogEventHashesForBatch(batchHash string) ([]string, error) {
-	rows, err := ses.db.Query(
+func (ses *SyncedEventsStore) Commit() {
+	err := ses.tx.Commit()
+	if err != nil {
+		log.Fatalf("Error while commiting transaction to db: %v", err)
+	}
+	ses.tx = nil
+}
+
+func (ses *SyncedEventsStore) Rollback() {
+	err := ses.tx.Rollback()
+	if err != nil {
+		log.Fatalf("Error while commiting transaction to db: %v", err)
+	}
+}
+
+func (ses *SyncedEventsStore) LogEventHashesForBatch(batchHash string) ([]string, error) {
+	rows, err := ses.tx.Query(
 		"SELECT event_hash FROM consumed_aiven_events "+
 			"WHERE batch_hash = $1;", batchHash)
 	if err != nil {
@@ -56,13 +74,20 @@ func (ses SyncedEventsStore) LogEventHashesForBatch(batchHash string) ([]string,
 	return eventHashesForBatch, nil
 }
 
-func (ses SyncedEventsStore) UpsertLogEvent(eventHash, batchHash string) (int64, error) {
+func (ses *SyncedEventsStore) UpsertLogEvent(eventHash, batchHash string) (int64, error) {
+	if ses.tx == nil {
+		tx, err := ses.db.BeginTx(context.Background(), nil) // TODO pass context down
+		if err != nil {
+			log.Fatalf("Error starting transaction with db: %v", err)
+		}
+		ses.tx = tx
+	}
+
 	query := "INSERT INTO consumed_aiven_events (event_hash, batch_hash) " +
 		"VALUES($1, $2) " +
 		"ON CONFLICT (event_hash) " +
 		"DO NOTHING;"
-
-	result, err := ses.db.Exec(query, eventHash, batchHash)
+	result, err := ses.tx.ExecContext(context.Background(), query, eventHash, batchHash)
 	if err != nil {
 		return 0, err
 	}
